c056334f483b73e56bfe8ef9542da0cbc8a321af
vpv79-dev <vakulin79@gmail.com>
2020-02-14 10:05:36 +0300

    added slider and product

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,18 @@
 $(function(){
 
+    $(".rate-star").rateYo({
+        rating: 5,
+        readOnly: true,
+        starWidth: "12px"
+      });
+
+      $(".product-slider__inner").slick({
+        dots: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        arrows: false,
+      });
+
     var mixer = mixitup('.products__inner-box');
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,8 @@ let gulp = require('gulp'),
         return gulp.src([
             'node_modules/normalize.css/normalize.css',
             'node_modules/slick-carousel/slick/slick.css',
-            'node_modules/magnific-popup/dist/magnific-popup.css'
+            'node_modules/magnific-popup/dist/magnific-popup.css',
+            'node_modules/rateyo/src/jquery.rateyo.css'
         ])
             .pipe(concat('libs.min.css'))
             .pipe(cssmin())
@@ -33,7 +34,8 @@ let gulp = require('gulp'),
         return gulp.src([
             'node_modules/slick-carousel/slick/slick.js',
             'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
-            'node_modules/mixitup/dist/mixitup.js'
+            'node_modules/mixitup/dist/mixitup.js',
+            'node_modules/rateyo/src/jquery.rateyo.js'
         ])
             .pipe(concat('libs.min.js'))
             .pipe(uglify())
