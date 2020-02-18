f0ad1c0defc3165688018aa973012df8f2b6a9b7
vpv79-dev <vakulin79@gmail.com>
2020-02-18 15:14:15 +0300

    added new-items

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,25 @@
 $(function(){
 
     $(".rate-star").rateYo({
-        rating: 5,
-        readOnly: true,
-        starWidth: "12px"
-      });
-
-      $(".product-slider__inner").slick({
-        dots: true,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        arrows: false,
-      });
+      rating: 5,
+      readOnly: true,
+      starWidth: "12px"
+    });
+    $(".product-slider__inner").slick({
+      dots: true,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      arrows: false,
+    });
+    $(".js-range-slider").ionRangeSlider({
+      type: "double",
+      grid: false,
+      min: 0,
+      max: 1000,
+      from: 0,
+      to: 600,
+      prefix: "$"
+    });
 
     var mixer = mixitup('.products__inner-box');
 
